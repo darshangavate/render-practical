ba3e1a062e46b5aaaabcba9d50230954c4d55087
Darshan gavate <darshangavate9529@gmail.com>
2026-05-07 07:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,52 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Cloud"
+    return """
+    <html>
+    <head>
+        <title>Cloud App</title>
+
+        <style>
+            body{
+                background:#0f172a;
+                color:white;
+                text-align:center;
+                font-family:Arial;
+                padding-top:120px;
+            }
+
+            .box{
+                background:#1e293b;
+                width:400px;
+                margin:auto;
+                padding:30px;
+                border-radius:15px;
+                box-shadow:0px 0px 15px cyan;
+            }
+
+            h1{
+                color:cyan;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <div class="box">
+            <h1>Hello Cloud ☁️</h1>
+            <p>Flask App Successfully Deployed on Render</p>
+        </div>
+
+    </body>
+    </html>
+    """
+
+@app.route("/about")
+def about():
+    return "Cloud Computing Practical using Flask + Render"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
